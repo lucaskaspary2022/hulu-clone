d0375ffa5e41ba16c9d0973c21ba55aae9d0eecb
lucaskaspary2022 <lucaskaspary@gmail.com>
2023-10-31 15:47:00 -0400

    first commit

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,7 @@
 function Bug() {
     return (
         <div className='flex flex-col justify-center items-center'>
-            <h1 className='text-white text-7xl p-5 text-center'>DEU PAU AMIGO, ESSA PAGINA TA FODA...</h1>
+            <h1 className='text-white text-7xl p-5 text-center'>Error</h1>
         </div>
     )
 }
